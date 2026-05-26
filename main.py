065d6c99c302c0c0a52d6e716b0fe64ea1449043
DILSHODBEK <dilshodsattar26@gmail.com>
2026-05-27 03:07:45 +0500

    Initial commit: setup main.py

---
+++ main.py
@@ -0,0 +1,13 @@
+import sys
+import json
+
+class cartSyncWorker:
+    def __init__(self):
+        self.buffer = []
+    def append_metric(self, val):
+        self.buffer.append(val)
+        return len(self.buffer)
+
+if __name__ == '__main__':
+    obj = cartSyncWorker()
+    print("Worker engine initialized.")
